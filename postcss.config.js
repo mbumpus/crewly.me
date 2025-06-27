eb5bfc79e43e80cc959a3ffc98d0a7e6078feaa3
AI Cabinet Method LLC <mike.bumpus@outlook.com>
2025-06-27 16:15:49 -0500

    Create postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,15 @@
+// File: postcss.config.js
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
+
+// File: next.config.js
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    domains: [],
+  },
+};
